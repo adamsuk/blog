480f8c5c29c63e8ef5532c1a844074969f2322dc
adamsuk <git-sync@trilby.media>
2022-02-28 20:37:02 +0000

    (Grav GitSync) Automatic Commit from adamsuk

---
+++ themes/blog2/blog2.php
@@ -0,0 +1,9 @@
+<?php
+namespace Grav\Theme;
+
+use Grav\Common\Theme;
+
+class Blog2 extends Theme
+{
+    // Access plugin events in this class
+}
--- themes/tutorial/tutorial.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Grav\Theme;
-
-use Grav\Common\Grav;
-use Grav\Common\Theme;
-
-class Tutorial extends Learn2 {
-
-}
